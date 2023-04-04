803e2c526b210742758a05d28be604d8adfa1775
kristina-sparrow <118679841+kristina-sparrow@users.noreply.github.com>
2023-04-04 17:32:59 -0600

    Finish nested collections exercises

--- 12_nested_collections/exercises/nested_array_exercises.rb
+++ 12_nested_collections/exercises/nested_array_exercises.rb
@@ -11,41 +11,55 @@ def blank_seating_chart(number_of_rows, seats_per_row)
 
   # NOTE: if one of the nested arrays is changed, the others should **not**
   # change with it
+  seating_chart = Array.new(number_of_rows) { Array.new(seats_per_row, nil) }
+  seating_chart
 end
 
 def add_seat_to_row(chart, row_index, seat_to_add)
   # take a chart (2d array)  and add seat_to_add to the end of the row that is
+  chart[row_index] << seat_to_add
   # at row_index index of the chart, then return the chart
+  chart
 end
 
 def add_another_row(chart, row_to_add)
   # take a chart and add row_to_add to the end of the chart,
+  chart << row_to_add
   # then return the chart.
+  chart
 end
 
 def delete_seat_from_row(chart, row_index, seat_index)
   # take a chart and delete the seat at seat_index of the row at row_index of
   # the chart, then return the chart
-
   # Hint: explore the ruby docs to find a method for deleting from an array!
+  chart[row_index].delete_at(seat_index)
+  chart
 end
 
 def delete_row_from_chart(chart, row_index)
   # take a chart and delete the row at row_index of the chart,
+  chart.delete_at(row_index)
   # then return the chart
+  chart
 end
 
 def count_empty_seats(chart)
   # take a chart and return the number of empty (nil) seats in it
-
   # NOTE: `chart` should **not** be mutated
+  chart.flatten.count(nil)
 end
 
 def find_favorite(array_of_hash_objects)
   # take an array_of_hash_objects and return the hash which has the key/value
   # pair :is_my_favorite? => true. If no hash returns the value true to the key
   # :is_my_favorite? it should return nil
-
+  array_of_hash_objects.each do |hash|
+    if hash[:is_my_favorite?] == true
+      return hash
+    end
+  end
+  return nil
   # array_of_hash_objects will look something like this:
   # [
   #   { name: 'Ruby', is_my_favorite?: true },
